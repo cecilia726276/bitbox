6e2281144ca783d67eac7fdd670e5898205e08cd
cecilia726276 <cecilia726276@outlook.com>
2019-03-29 09:51:50 +1100

    TCP sockets

--- src/main/java/unimelb/bitbox/Peer.java
+++ src/main/java/unimelb/bitbox/Peer.java
@@ -1,10 +1,21 @@
 package unimelb.bitbox;
 
+import unimelb.bitbox.util.Configuration;
+
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.logging.Logger;
 
-import unimelb.bitbox.util.Configuration;
+//        Peer Protocol Messages:
+//        INVALID_PROTOCOL
+//        CONNECTION_REFUSED
+//        HANDSHAKE_REQUEST, HANDSHAKE_RESPONSE
+//        FILE_CREATE_REQUEST, FILE_CREATE_RESPONSE
+//        FILE_DELETE_REQUEST, FILE_DELETE_RESPONSE
+//        FILE_MODIFY_REQUEST, FILE_MODIFY_RESPONSE
+//        DIRECTORY_CREATE_REQUEST, DIRECTORY_CREATE_RESPONSE
+//        DIRECTORY_DELETE_REQUEST, DIRECTORY_DELETE_RESPONSE
+//        FILE_BYTES_REQUEST, FILE_BYTES_RESPONSE
 
 public class Peer 
 {
@@ -15,7 +26,7 @@ public class Peer
                 "[%1$tc] %2$s %4$s: %5$s%n");
         log.info("BitBox Peer starting...");
         Configuration.getConfiguration();
-        
+
         new ServerMain();
         
     }
--- src/main/java/unimelb/bitbox/ServerMain.java
+++ src/main/java/unimelb/bitbox/ServerMain.java
@@ -1,13 +1,13 @@
 package unimelb.bitbox;
 
+import unimelb.bitbox.util.Configuration;
+import unimelb.bitbox.util.FileSystemManager;
+import unimelb.bitbox.util.FileSystemManager.FileSystemEvent;
+import unimelb.bitbox.util.FileSystemObserver;
+
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.logging.Logger;
-
-import unimelb.bitbox.util.Configuration;
-import unimelb.bitbox.util.FileSystemManager;
-import unimelb.bitbox.util.FileSystemObserver;
-import unimelb.bitbox.util.FileSystemManager.FileSystemEvent;
 
 public class ServerMain implements FileSystemObserver {
 	private static Logger log = Logger.getLogger(ServerMain.class.getName());
@@ -16,10 +16,12 @@ public class ServerMain implements FileSystemObserver {
 	public ServerMain() throws NumberFormatException, IOException, NoSuchAlgorithmException {
 		fileSystemManager=new FileSystemManager(Configuration.getConfigurationValue("path"),this);
 	}
-
+// asychronize (blocking method, use 1 or 2 of the threads to process the received message  )
 	@Override
 	public void processFileSystemEvent(FileSystemEvent fileSystemEvent) {
+
 		// TODO: process events
+
 	}
 	
 }
