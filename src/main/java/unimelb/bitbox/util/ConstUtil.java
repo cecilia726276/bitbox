b3deff4240fa9146d73c598f9c84cfaf142421c0
cecilia726276 <cecilia726276@outlook.com>
2019-05-02 20:22:40 +1000

    constant

--- src/main/java/unimelb/bitbox/util/ConstUtil.java
+++ src/main/java/unimelb/bitbox/util/ConstUtil.java
@@ -1,13 +1,21 @@
 package unimelb.bitbox.util;
 
 public class ConstUtil {
+    public static final String INVALID_PROTOCOL = "INVALID_PROTOCOL";
+    public static final String CONNECTION_REFUSED = "CONNECTION_REFUSED";
+    public static final String HANDSHAKE_REQUEST = "HANDSHAKE_REQUEST";
+    public static final String FILE_CREATE_REQUEST = "FILE_CREATE_REQUEST";
     public static final String FILE_CREATE_RESPONSE = "FILE_CREATE_RESPONSE";
+    public static final String FILE_MODIFY_REQUEST = "FILE_MODIFY_REQUEST";
     public static final String FILE_MODIFY_RESPONSE = "FILE_MODIFY_RESPONSE";
     public static final String FILE_BYTES_REQUEST = "FILE_BYTES_REQUEST";
     public static final String FILE_BYTES_RESPONSE = "FILE_BYTES_RESPONSE";
+    public static final String FILE_DELETE_REQUEST = "FILE_DELETE_REQUEST";
+    public static final String FILE_DELETE_RESPONSE = "FILE_DELETE_RESPONSE";
     public static final int MAXIMUM_INCOMMING_CONNECTIONS = Integer.parseInt(Configuration.getConfigurationValue("maximumIncommingConnections"));
     public static final String IP = Configuration.getConfigurationValue("advertisedName");
     public static final int PORT = Integer.parseInt(Configuration.getConfigurationValue("port"));
+    public static final int BLOCKSIZE = Integer.parseInt(Configuration.getConfigurationValue("blockSize"));
 
 
 }
